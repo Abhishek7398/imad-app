e2da5a4426e16810c3ce2d93f9f1970459508de3
Abhishek7398 <shirsatabhishek1@gmail.com>
2017-08-25 12:15:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One | Abhishek Patil',
+    heading: 'Article One',
+    date: '25 August 2017',
+    content:
+                       `<p>
+                            THis is the content of my first article and I am going to write this with my full devotion and I am not goin gto make any mistake in my codin and I will always attend the lectures of coding properly and I am going to score more marks as looking towards y practice.THis is the content of my first article and I am going to write this with my full devotion and I am not goin gto make any mistake in my codin and I will always attend the lectures of coding properly and I am going to score more marks as looking towards y practice.
+                            
+                        </p>
+                        <p>
+                            THis is the content of my first article and I am going to write this with my full devotion and I am not goin gto make any mistake in my codin and I will always attend the lectures of coding properly and I am going to score more marks as looking towards y practice.THis is the content of my first article and I am going to write this with my full devotion and I am not goin gto make any mistake in my codin and I will always attend the lectures of coding properly and I am going to score more marks as looking towards y practice.
+                            
+                        </p>
+                        <p>THis is the content of my first article and I am going to write this with my full devotion and I am not goin gto make any mistake in my codin and I will always attend the lectures of coding properly and I am going to score more marks as looking towards y practice.THis is the content of my first article and I am going to write this with my full devotion and I am not goin gto make any mistake in my codin and I will always attend the lectures of coding properly and I am going to score more marks as looking towards y practice.
+                        </p>`
+};
+ function createTemplate(data) {
+     var title = data.title;
+     var date = data.date;
+     var heading = data.heading;
+     var content = data.content;
+ 
+var htmlTemplate = `
+<html>
+    <head>
+        <title> 
+        ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            
+             <div>
+                <a href= "/">Home</a> 
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                 </div>
+            </div>
+    </body>
+</html>`;
+
+return htmlTemplate ; 
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(atricleOne));
 });
 
 app.get('/article-two', function(req,res) {
